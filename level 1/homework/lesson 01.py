cba1009e2e4562b19ec28e1ee3bd42e25a1522b7
giorgigamsakhurdia <gamsakhurdiag357@gmail.com>
2024-05-24 22:28:17 +0400

    commit

--- level 1/homework/lesson 01.py
+++ level 1/homework/lesson 01.py
@@ -1,5 +1,8 @@
-x = "girogi"
+x = "giorgi"
 y = 10
 z = 4
+d = 5.5
 print("me" + " var " + x)
-print("me " + " var " + str(y+z))
+print("me " + "var " + str(y+z) + " wlis")
+print(str(d) + " kargi ricxvia")
+print("me shemovedi goashi radgan chemi programrebis donis ganvitareba minda")
