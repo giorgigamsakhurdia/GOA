11516268b45b016a7daa7c66d6c788175685fb7d
giorgigamsakhurdia <gamsakhurdiag357@gmail.com>
2024-05-25 15:21:30 +0400

    commit

---
+++ level 3/homework/lesson 03.py
@@ -0,0 +1,20 @@
+
+#metamatikuri gamotvlebi
+print(5+5)
+print(5*5)
+print(5-5)
+
+#roca 5 stringad vaqcevs terminali gveubneba rom  str da int ertad ver gavyoft
+print("5"/5) #
+
+#es dawers me mqvia giorgis
+
+print("me" + " mqvia" + " giorgi")
+
+#terminali aris adgili sadac chveni dawerili brdzanebebi sruldeba
+
+#ori cvladis jami
+a=4
+b=3
+print(a+b)
+
