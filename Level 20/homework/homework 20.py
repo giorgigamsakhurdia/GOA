a9a8eb3fb9a1df3947e7bbe3eb3ef3c97f15c73f
giorgigamsakhurdia <gamsakhurdiag357@gmail.com>
2024-06-26 14:34:24 +0400

    Add files via upload

---
+++ Level 20/homework/homework 20.py
@@ -0,0 +1,20 @@
+def hello(name):
+    print(name)
+
+hello("GOA Best!")
+
+def sum(arg1, arg2):
+    print(arg1 + arg2)
+sum(3, 6)
+
+def to_string(value):
+    return type(str(value))
+print(to_string(23132.5612))
+
+def print_type(valu):
+    return type(valu)
+print(print_type(5 > 7))
+
+def to_integer(val):
+    return int(val)
+print(to_integer(5.6))
