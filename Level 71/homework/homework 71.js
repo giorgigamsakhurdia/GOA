db92850fa3bea7a47c1cc58ae0233498dcdb8075
giorgigamsakhurdia <gamsakhurdiag357@gmail.com>
2024-11-02 11:45:01 +0400

    Add files via upload

---
+++ Level 71/homework/homework 71.js
@@ -0,0 +1,138 @@
+//first 
+
+function random() {
+    return Math.round(Math.random() * 99) + 1;
+}
+random()
+
+//second
+
+function circlearea(radius) {
+    return Math.PI * radius * radius;
+}
+
+curclearea()
+
+//Math.PI  pi = 3.14..
+
+console.log(Math.PI);
+console.log(Math.PI * 2);
+console.log(Math.PI * 4);
+console.log(Math.PI - 2);
+console.log(Math.PI + 1);
+console.log(Math.PI * Math.PI);
+console.log(Math.PI / 2);
+console.log(Math.PI * 3);
+console.log(Math.PI - 5);
+console.log(Math.PI * 10);
+
+//Math.E E = 2.71...
+
+console.log(Math.E);
+console.log(Math.E ** 2);
+console.log(Math.E ** 3);
+console.log(Math.E - 1);
+console.log(Math.E * 10);
+console.log(Math.E + 6);
+console.log(Math.E / 2.4);
+console.log(Math.E + 5);
+console.log(Math.E / 2);
+console.log(Math.E * Math.E);
+
+//Math.floor დამრგვალება ყველაზე ახლო ქვედა რიცხვი
+
+console.log(Math.floor(4.7));
+console.log(Math.floor(-4.7));
+console.log(Math.floor(5.0));
+console.log(Math.floor(0.9));
+console.log(Math.floor(-0.1));
+console.log(Math.floor(3.14));
+console.log(Math.floor(9.999));
+console.log(Math.floor(10.1));
+console.log(Math.floor(100.99));
+console.log(Math.floor(-10.5));
+
+//Math.trunc
+
+//??? კარგად ვერ გავიგე
+
+//Math.round პროსტა დამრგვალება
+
+
+console.log(Math.round(4.7));
+console.log(Math.round(-4.7));
+console.log(Math.round(5.0));
+console.log(Math.round(0.9));
+console.log(Math.round(-0.1));
+console.log(Math.round(3.14));
+console.log(Math.round(9.5));
+console.log(Math.round(10.1));
+console.log(Math.round(100.49));
+console.log(Math.round(-10.5));
+
+//Math.ceil დამრგვალება ყველაზე მაღალ რიცხვზე
+
+console.log(Math.ceil(4.7));
+console.log(Math.ceil(-4.7));
+console.log(Math.ceil(5.0));
+console.log(Math.ceil(0.1));
+console.log(Math.ceil(-0.1));
+console.log(Math.ceil(3.14));
+console.log(Math.ceil(9.001));
+console.log(Math.ceil(10.1));
+console.log(Math.ceil(100.01));
+console.log(Math.ceil(-10.5));
+
+
+//Math.pow  რიცხვი, რომელ ხარისხში ადის
+
+console.log(Math.pow(2, 3));
+console.log(Math.pow(3, 2));
+console.log(Math.pow(5, 0));
+console.log(Math.pow(10, 2));
+console.log(Math.pow(7, -1));
+console.log(Math.pow(4, 0.5));
+console.log(Math.pow(10, 3));
+console.log(Math.pow(2, 10));
+console.log(Math.pow(1.5, 2));
+console.log(Math.pow(-3, 2));
+
+
+//Math.random random
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+//Math.abs იმენა ეგ რიცხვი
+
+console.log(Math.abs(4.7));
+console.log(Math.abs(-4.7));
+console.log(Math.abs(5.0));
+console.log(Math.abs(-5.0));
+console.log(Math.abs(0.9));
+console.log(Math.abs(-0.1));
+console.log(Math.abs(-3.14));
+console.log(Math.abs(9.999));
+console.log(Math.abs(-10));
+console.log(Math.abs(-100.5));
+
+//Math.sqrt ფესვი
+
+console.log(Math.sqrt(4));
+console.log(Math.sqrt(9));
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(25));
+console.log(Math.sqrt(2));
+console.log(Math.sqrt(0));
+console.log(Math.sqrt(1));
+console.log(Math.sqrt(10));
+console.log(Math.sqrt(49));
+console.log(Math.sqrt(100));
