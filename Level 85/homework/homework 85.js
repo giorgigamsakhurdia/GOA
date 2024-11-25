d6704cbbe30d13932d249e0740e72fecc650ff43
giorgigamsakhurdia <gamsakhurdiag357@gmail.com>
2024-11-25 17:03:48 +0400

    Add files via upload

---
+++ Level 85/homework/homework 85.js
@@ -0,0 +1,11 @@
+document.getElementsByClassName("div")[0].addEventListener("click", function() {
+    console.log("div")
+})
+
+document.getElementsByClassName("div1")[0].addEventListener("click", function() {
+    console.log("div1")
+})
+
+document.getElementsByClassName("button")[0].addEventListener("click", function() {
+    console.log("button")
+})
