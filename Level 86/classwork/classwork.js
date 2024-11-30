a1a73ffe458da58b0a016f420ea42f8157a2098d
giorgigamsakhurdia <gamsakhurdiag357@gmail.com>
2024-11-30 20:29:37 +0400

    Add files via upload

---
+++ Level 86/classwork/classwork.js
@@ -0,0 +1,7 @@
+function changpic() {
+    document.getElementById("a").src=("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmuxG3N6SFwkP4u0UxaxSN7NMdJ7r5PYjlpQ&s");
+}
+
+function changpic2() {
+    document.getElementById("a").src=("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSgARldygWkeUJSXhp-YQNbV7UoTrC9onwqA&s");
+}
---
+++ Level 86/homework/homework 86.js
@@ -0,0 +1,52 @@
+// let a = [5, 1, 25, 6, 2]
+// console.log(a[1])
+// console.log(a[3])
+
+
+// let N = prompt("write N: ")
+// let numbers = []
+
+// for(let i = 0; i< N; i++){
+//     numbers.push(prompt('write number: '))
+// }
+
+// for (let i = 1; i < N; i += 2){
+//     console.log(numbers[i])
+// }
+
+
+// let b = []
+// for (let i = 0; i < 11; i++){
+//     b.push(i)
+// }
+// let dog = []
+// for(let n = 0; )
+
+
+// let n = prompt("wrote N: ")
+// let nums = []
+// for(let g = 0; g < N; g++){
+//     nums.push(prompt('write number: '))
+// }
+
+
+// let x = prompt("write N: ")
+// let num = []
+
+// for(let f = 0; f < x; f++){
+//     num.push(prompt('write number: '))
+// }
+
+// const yes = num[0];
+// num[0] = num[x - 1];
+// num[x - 1] = yes;
+
+// console.log(num)
+
+function changpic() {
+    document.getElementById("a").src=("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmuxG3N6SFwkP4u0UxaxSN7NMdJ7r5PYjlpQ&s");
+}
+
+function changpic2() {
+    document.getElementById("a").src=("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSgARldygWkeUJSXhp-YQNbV7UoTrC9onwqA&s");
+}
